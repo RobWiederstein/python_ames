8568861526e69df3abc7c3c348375ce7025a2f2f
RobWiederstein <khuon68@gmail.com>
2022-09-20 20:42:06 -0400

    add more from kaggle script

--- 01_script.py
+++ 01_script.py
@@ -12,9 +12,67 @@ from sklearn.preprocessing import StandardScaler
 from scipy import stats
 
 import warnings
-warnings.filterwarnings('ignore')
-#%matplotlib inline
 
-#import pandas as pd
-#bring in the six packs
+warnings.filterwarnings('ignore')
+# %matplotlib inline
+
+# bring in the six packs
 df_train = pd.read_csv('./input/train.csv')
+
+# check the decoration
+df_train.columns
+
+# descriptive statistics summary
+df_train['SalePrice'].describe()
+
+# histogram
+sns.distplot(df_train['SalePrice'])
+
+# skewness and kurtosis
+print("Skewness: %f" % df_train['SalePrice'].skew())
+print("Kurtosis: %f" % df_train['SalePrice'].kurt())
+
+# scatter plot grlivarea/saleprice
+var = 'Gr Liv Area'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis = 1)
+data.plot.scatter(x = var, y = 'SalePrice', ylim = (0, 800000))
+
+# scatter plot totalbsmtsf/saleprice
+var = 'Total Bsmt SF'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis = 1)
+data.plot.scatter(x = var, y = 'SalePrice', ylim = (0, 800000))
+
+# box plot overallqual/saleprice
+var = 'Overall Qual'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis = 1)
+f, ax = plt.subplots(figsize = (8, 6))
+fig = sns.boxplot(x = var, y = "SalePrice", data = data)
+fig.axis(ymin = 0, ymax = 800000)
+
+# box plot year built
+var = 'Year Built'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis = 1)
+f, ax = plt.subplots(figsize = (16, 8))
+fig = sns.boxplot(x = var, y = "SalePrice", data = data)
+fig.axis(ymin = 0, ymax = 800000)
+plt.xticks(rotation = 90)
+
+# correlation matrix
+corrmat = df_train.corr()
+f, ax = plt.subplots(figsize = (12, 9))
+sns.heatmap(corrmat, vmax = .8, square = True)
+
+# saleprice correlation matrix
+k = 10  # number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(df_train[cols].values.T)
+sns.set(font_scale = 1.25)
+hm = sns.heatmap(cm, cbar = True, annot = True, square = True, fmt = '.2f', annot_kws = {'size': 10},
+                 yticklabels = cols.values, xticklabels = cols.values)
+plt.show()
+
+# scatterplot
+sns.set()
+cols = ['SalePrice', 'Overall Qual', 'Gr Liv Area', 'Garage Cars', 'Total Bsmt SF', 'Full Bath', 'Year Built']
+sns.pairplot(df_train[cols], size = 2.5)
+plt.show()
